f935bd6c01bc99ae930076cc06deaedb6423e45b
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2017-04-29 00:08:08 +0300

    Shadows with meshes

---
+++ shaders-project/Assets/Obstacle.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	List<Vector3> verts = new List<Vector3>();
+	List<Vector3> verts2D = new List<Vector3>();
+
+	List<Transform> visuals = new List<Transform>();
+
+	MeshFilter filter;
+
+	void Awake () {
+		filter = GetComponent<MeshFilter>();
+	}
+
+	void Start () {
+		var tempVerts = filter.mesh.vertices;
+		foreach (Vector3 v in tempVerts) {
+			var tempVert = v;
+			if (!verts.Contains(tempVert)) {
+				if (!ContainsYExcluded(verts2D, tempVert)) {
+					var visual = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					visual.transform.localScale = Vector3.one * 0.1f;
+					visuals.Add(visual.transform);
+					verts2D.Add(new Vector3(tempVert.x, 0, tempVert.z));
+				}
+				verts.Add(tempVert);
+			}
+		}
+	}
+
+	void Update () {
+		for (int i = 0; i < verts2D.Count; i++) {
+			var vertWorld = GetWorldPos(verts2D[i]);
+			vertWorld.y = 0;
+			visuals[i].position = vertWorld;
+		}
+	}
+
+	Vector3 GetWorldPos (Vector3 v) {
+		var result = v;
+		// Scale
+		result.x *= transform.lossyScale.x;
+		result.y *= transform.lossyScale.y;
+		result.z *= transform.lossyScale.z;
+		// Rotation
+		result = transform.rotation * result;
+		// World pos
+		result += transform.position;
+
+		return result;
+	}
+
+	bool ContainsYExcluded (List<Vector3> list, Vector3 comp) {
+		foreach (Vector3 v in list) {
+			if (v.x == comp.x && v.z == comp.z) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public List<Vector3> Get2DVerts () {
+		return verts2D;
+	}
+
+	public List<Vector3> GetWorld2DVerts () {
+		var worldVerts = new List<Vector3>();
+		foreach (Vector3 v in verts2D) {
+			var worldV = GetWorldPos(v);
+			worldV.y = 0;
+			worldVerts.Add(worldV);
+		}
+		return worldVerts;
+	}
+}
---
+++ shaders-project/Assets/PlayerLineOfSight.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLineOfSight : MonoBehaviour {
+
+	enum HitInfo { None, Left, Right, Both }
+
+	struct VertexInfo {
+		public HitInfo hitInfo;
+		public Vector3 dirFromPlayer;
+
+		public VertexInfo (HitInfo info, Vector3 dir) {
+			hitInfo = info;
+			dirFromPlayer = dir;
+		}
+	}
+
+	public LayerMask mask;
+	public Material shadowMat;
+	public float forkAngle;
+	public float distPad;
+	public float shadowRadius;
+
+	List<Obstacle> obstacles;
+	List<GameObject> shadows = new List<GameObject>();
+	int shadowIndex;
+
+	void Start () {
+		obstacles = new List<Obstacle>(FindObjectsOfType<Obstacle>());
+		foreach (Obstacle o in obstacles) {
+			var shadow = new GameObject("Shadow");
+			shadow.AddComponent<MeshFilter>();
+			shadow.AddComponent<MeshRenderer>();
+			shadows.Add(shadow);
+		}
+	}
+	
+	void Update () {
+		foreach (Obstacle o in obstacles) {
+			var verts = o.GetWorld2DVerts();
+			var quadVerts = new Vector3[4];
+			foreach (Vector3 v in verts) {
+				var hit = DoubleCast(transform.position, v, o);
+				if (hit.hitInfo == HitInfo.Left) {
+					quadVerts[0] = v;
+					quadVerts[3] = transform.position + hit.dirFromPlayer * shadowRadius;
+				}
+				if (hit.hitInfo == HitInfo.Right) {
+					quadVerts[1] = v;
+					quadVerts[2] = transform.position + hit.dirFromPlayer * shadowRadius;
+				}
+			}
+			GenerateQuad(quadVerts);
+		}
+		shadowIndex = 0;
+	}
+
+	VertexInfo DoubleCast (Vector3 origin, Vector3 end, Obstacle obst) {
+		HitInfo info = HitInfo.None;
+		var dir = (end - origin).normalized;
+		var dist = (end - origin).magnitude;
+
+		var left = Quaternion.AngleAxis(forkAngle / 2, Vector3.up) * dir;
+		var right = Quaternion.AngleAxis(-forkAngle / 2, Vector3.up) * dir;
+
+		var hits = new List<RaycastHit>(Physics.RaycastAll(origin, left, dist + distPad, mask));
+		foreach (RaycastHit hit in hits) {
+			if (hit.collider.GetComponent<Obstacle>() == obst) {
+				info = HitInfo.Left;
+			}
+		}		
+		
+		// DEBUG
+		//if (info == HitInfo.Left) {
+		//	Debug.DrawLine(origin, origin + left * (dist + distPad), Color.red);
+		//} else {
+		//	Debug.DrawLine(origin, origin + left * (dist + distPad), Color.green);
+		//}
+
+		hits = new List<RaycastHit>(Physics.RaycastAll(origin, right, dist + distPad, mask));
+		foreach (RaycastHit hit in hits) {
+			if (hit.collider.GetComponent<Obstacle>() == obst) {
+				if (info == HitInfo.Left) {
+					info = HitInfo.Both;
+				} else {
+					info = HitInfo.Right;
+				}
+			}
+		}
+
+		// DEBUG
+		//if (info == HitInfo.Right || info == HitInfo.Both) {
+		//	Debug.DrawLine(origin, origin + right * (dist + distPad), Color.red);
+		//} else {
+		//	Debug.DrawLine(origin, origin + right * (dist + distPad), Color.green);
+		//}
+
+		return new VertexInfo(info, dir);
+	}
+
+	void GenerateQuad (Vector3[] vertices) {
+		var quad = shadows[shadowIndex];
+
+		var mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = new int[] { 2, 1, 0, 2, 0, 3 };
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		var mf = quad.GetComponent<MeshFilter>();
+		var mr = quad.GetComponent<MeshRenderer>();
+		mf.mesh = mesh;
+		mr.material = shadowMat;
+
+		shadowIndex++;
+	}
+}
